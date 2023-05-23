8c89cd9f8fe601c99a62deef1fe6e2fc86f2e721
juan ramirez arias <jcra.juancamilo@gmail.com>
2023-05-22 23:51:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gato gato = new Gato();
+            gato.HacerRuido();
+
+            Perro perro = new Perro();
+            perro.HacerRuido();
+
+
+        }
+    }
+}
